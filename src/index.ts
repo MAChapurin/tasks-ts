192d1b57f57782c2afd5cf0387d8886af8beddd2
Michael <machapurin@icloud.com>
2023-08-01 00:55:35 +0300

    project finished

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-interface IButton {
-  bg: string;
-  sound: string;
-}
-
 const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.btn');
 const audios: NodeListOf<HTMLAudioElement> = document.querySelectorAll('audio')
-const body = document.body;
+const body: HTMLElement = document.body;
+
 btns.forEach((btn) => btn.addEventListener('click', ()=> {
   body.setAttribute('style', `background-image: url('../assets/${btn.dataset['bg']}.jpg');`);
-  console.log(document.querySelector(`[data-sound="${btn.dataset['link']}]"`));
-  console.log(btn.dataset['link'])
-  console.log(document.querySelector(`[data-sound="${btn.dataset['link']}"]`));
-  const audio: HTMLAudioElement | null = document.querySelector(`[data-sound="${btn.dataset['link']}"]`);
   audios.forEach(track => track.pause());
-  audio?.play();
+
+  const audio: HTMLAudioElement | null  = document.querySelector(`[data-sound="${btn.dataset['link']}"]`);
+  if(!audio) return; 
+
+  if(audio.dataset['state'] === 'stop') {
+    audio.setAttribute('data-state', 'play');
+    audio.play();
+  } else {
+    audio.pause();
+    audio.setAttribute('data-state', 'stop');
+  }
 }))
+
+const input: HTMLInputElement | null = document.querySelector('.input');
+if (input) {
+  input.addEventListener('change', ()=> {
+    audios.forEach(track => track.volume = +input.value);
+  })
+}
